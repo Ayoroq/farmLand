877a9a4b14879f8a1b23829eee786a7ee0878f32
Ayo <Ayoroq@gmail.com>
2025-11-18 23:20:15 -0500

    feat: integrate react-router for improved navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes/Approute.jsx";
 import App from './App.jsx'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+  </StrictMode>
 )
