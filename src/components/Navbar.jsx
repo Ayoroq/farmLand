b08f386572b5640a7a367c69d39399320c345942
Ayo <Ayoroq@gmail.com>
2025-11-20 16:42:18 -0500

    feat: implement responsive Navbar with toggle menu functionality and add new SVG icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,48 @@
 import { Link } from "react-router";
+import { useState, useEffect } from "react";
 import style from "./Navbar.module.css";
 
 export default function Navbar() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  const toggleMenu = () => {
+    const newMenuState = !isMenuOpen;
+    setIsMenuOpen(newMenuState);
+    const menu = document.querySelector(`.${style.menubtn}`);
+    if (newMenuState) {
+      menu.classList.add(style.open);
+    } else {
+      menu.classList.remove(style.open);
+    }
+  };
+
+  if (screenWidth < 530) {
+    return (
+      <nav className={style.navbar}>
+        <ul className={style.nav}>
+          <button className={style.menubtn} onClick={toggleMenu}>
+            <span className={style.span}></span>
+            <span className={style.span}></span>
+            <span className={style.span}></span>
+          </button>
+          <button className={style.menubtn}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <li className={style.logo}>
+            <Link to="/">FarmLand</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
   return (
     <nav className={style.navbar}>
       <ul className={style.leftNav}>
