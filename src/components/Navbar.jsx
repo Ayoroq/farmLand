6adaa50dd2c47518e3cbdd4f7de7527d67f69507
Ayo <Ayoroq@gmail.com>
2025-11-21 03:05:24 -0500

    feat: enhance Navbar responsiveness by closing sidebar on screen resize and adjusting sidebar max-width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,22 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => setScreenWidth(window.innerWidth);
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      setScreenWidth(newWidth);
+      
+      // Close sidebar when screen becomes larger than 620px
+      if (newWidth >= 720 && isMenuOpen) {
+        const sideBar = document.querySelector(`.${style.sideBar}`);
+        setIsMenuOpen(false);
+        if (sideBar) {
+          sideBar.classList.remove(style.open);
+        }
+      }
+    };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [isMenuOpen]);
 
   const toggleMenu = () => {
     const sideBar = document.querySelector(`.${style.sideBar}`);
@@ -56,30 +68,33 @@ export default function Navbar() {
     );
   }
   return (
-    <nav className={style.navbar}>
-      <ul className={style.leftNav}>
-        <li className={style.navItem}>
-          <Link to="/" className={style.logo}>
-            FarmLand
-          </Link>
-        </li>
-      </ul>
-      <ul className={style.rightNav}>
-        <li className={style.navItem}>
-          <Link to="/shop">Shop</Link>
-        </li>
-        <li className={style.navItem}>
-          <Link to="/about">Who we are</Link>
-        </li>
-        <li className={style.navItem}>
-          <Link to="/contact">Find Us</Link>
-        </li>
-        <li className={`${style.basketBtnContainer} ${style.navItem}`}>
-          <button className={style.basketBtn}>
-            <Link to="/basket">Basket</Link>
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={style.navbar}>
+        <ul className={style.leftNav}>
+          <li className={style.navItem}>
+            <Link to="/" className={style.logo}>
+              FarmLand
+            </Link>
+          </li>
+        </ul>
+        <ul className={style.rightNav}>
+          <li className={style.navItem}>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li className={style.navItem}>
+            <Link to="/about">Who we are</Link>
+          </li>
+          <li className={style.navItem}>
+            <Link to="/contact">Find Us</Link>
+          </li>
+          <li className={`${style.basketBtnContainer} ${style.navItem}`}>
+            <button className={style.basketBtn}>
+              <Link to="/basket">Basket</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <section className={style.sideBar}></section>
+    </>
   );
 }
