cef15ffed2755c7c3a0b7847e74c0b9e84ba2379
Ayo <Ayoroq@gmail.com>
2025-11-21 22:38:37 -0500

    feat: update HTML structure and styles for Home page, enhance Navbar z-index, and improve global scrollbar handling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import style from "./Home.module.css";
 
 export default function Home() {
-  return (
+  return (<>
     <main className={style.main}>
-      <h1>Welcome to FarmLand!</h1>
-      <p>Your one-stop shop for fresh produce and farm supplies.</p>
+      <h1 className={style.title}>We’re <span className={style.italic}>farmers</span>, <span className={style.italic}>purveyors</span>, and <span className={style.italic}>eaters</span> of <br/>organically grown food.</h1>
     </main>
+    </>
   );
 }
