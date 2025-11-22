65f83cf41d6eeabc9da92c74c8de25adfd1047bc
Ayo <Ayoroq@gmail.com>
2025-11-22 00:47:24 -0500

    Added a new button to the homepage to go to the store, and increased fonts for the title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,36 @@
 import styles from "./Home.module.css";
-import vegImage from "../../assets/HomeAssets/vegetables.png"
-import multiVegImage from "../../assets/HomeAssets/multiple-veg.png"
+import { Link } from "react-router";
+import vegImage from "../../assets/HomeAssets/vegetables.png";
+import multiVegImage from "../../assets/HomeAssets/multiple-veg.png";
 
 export default function Home() {
   return (
     <>
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        We’re <em className={styles.italic}>farmers</em>, <em className={styles.italic}>purveyors</em>, and <em className={styles.italic}>eaters</em> of organically grown food.
-      </h1>
-    </main>
-    <section className={`${styles.section} ${styles.section1}`}>
-          <div>
-            <img src={vegImage} alt="Green Vegetable leafs" />
-          </div>
-          <div>
-            <img src={multiVegImage} alt="Image of multiple images" />
-          </div>
-    </section>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          We’re <em>farmers</em>,<em> purveyors</em>, and <em>eaters</em>{" "}
+          <br></br>of organically grown food.
+        </h1>
+        <Link className={styles.shopButton} to="/shop">
+          Browse Our Shop
+        </Link>
+      </main>
+      <section className={`${styles.section} ${styles.section1}`}>
+        <figure>
+          <img src={vegImage} alt="Fresh organic green vegetables" />
+          <figcaption>Fresh organic vegetables grown locally.</figcaption>
+        </figure>
+
+        <figure>
+          <img src={multiVegImage} alt="A variety of organic produce" />
+          <figcaption>
+            A colorful variety of organic produce from our farms.
+            <br />
+            <b>Central California</b> — The person who grew these was located in
+            Central California and, er, hopefully very well-compensated.
+          </figcaption>
+        </figure>
+      </section>
     </>
   );
 }
