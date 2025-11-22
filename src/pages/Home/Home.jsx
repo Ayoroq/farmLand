38085e8cd95f7fb2d0f67ca422acdd1020814eb3
Ayo <Ayoroq@gmail.com>
2025-11-22 16:13:17 -0500

    Added a test video and content to the second section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,11 @@ import styles from "./Home.module.css";
 import { Link } from "react-router";
 import vegImage from "../../assets/HomeAssets/vegetables.png";
 import multiVegImage from "../../assets/HomeAssets/multiple-veg.png";
+import ArialViewVideo from "../../assets/HomeAssets/Aerial View of Farmland.mp4";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.home}>
       <main className={styles.main}>
         <h1 className={styles.title}>
           We’re <em>farmers</em>,<em> purveyors</em>, and <em>eaters</em>{" "}
@@ -31,6 +32,42 @@ export default function Home() {
           </figcaption>
         </figure>
       </section>
-    </>
+      <section className={`${styles.section} ${styles.section2}`}>
+        <video
+          src={ArialViewVideo}
+          autoPlay
+          muted
+          loop
+          type="video/mp4"
+        ></video>
+        <div className={styles.info}>
+          <h4>WHAT WE BELIEVE</h4>
+          <p>
+            We believe in produce. Tasty produce. Produce like:
+            <br />
+            <br />
+            Apples. Oranges.Limes. Lemons. Guavas. Carrots. Cucumbers. Jicamas.
+            Cauliflowers. Brussels sprouts. Shallots. Japanese eggplants.
+            Asparagus. Artichokes—Jerusalem artichokes, too. Radishes. Broccoli.
+            Baby broccoli. Broccolini. Bok choy. Scallions. Ginger. Cherries.
+            Raspberries. Cilantro. Parsley. Dill.
+            <br />
+            <br />
+            What are we forgetting?
+            <br />
+            <br />
+            Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”).
+            Persian cucumbers, in addition to aforementioned “normal” cucumbers.
+            Artichokes. Zucchinis. Pumpkins. Squash (what some cultures call
+            pumpkins). Sweet potatoes and potato-potatoes. Jackfruit. Monk
+            fruit. Fruit of the Loom. Fruits of our labor (this website).
+            Sorrel. Pineapple. Mango. Gooseberries. Blackberries. Tomatoes.
+            Heirloom tomatoes. Beets. Chives. Corn. Endive. Escarole, which, we
+            swear, we’re vendors of organic produce, but if you asked us to
+            describe what escaroles are...
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
