d6ab51134a02e4bc02148e0109b99ec2e801951a
Ayo <Ayoroq@gmail.com>
2025-11-23 07:22:56 -0500

    completed the horizontal scrolling effect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,34 +10,67 @@ import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export default function Home(){
-  const containerRef = useRef([])
+export default function Home() {
+  const sectionRef = useRef([]);
+  const homeRef = useRef(null);
 
   useGSAP(() => {
-    const Navbar = document.querySelector("#nav")
-    const NavbarHeight = Navbar.offsetHeight
-    containerRef.current.forEach((container) => {
-      ScrollTrigger.create({
-        trigger: container,
-        start: `top ${NavbarHeight}`,
+    const Navbar = document.querySelector("#nav");
+    const NavbarHeight = Navbar.offsetHeight;
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: homeRef.current,
+        start: `top ${NavbarHeight}px`,
+        end: '+=' + ((sectionRef.current.length - 1) * 100) + "%",
+        scrub: true,
         pin: true,
-        pinSpacing: false,
-        markers: true
-      })
-  })
-});
+        pinSpacing: true,
+      },
+    });
 
-  return(
-    <div className={styles.home}>
-      <section className={`${styles.hero} ${styles.section} ${styles.red}`} ref={(el) => {if(el && !containerRef.current.includes(el)){containerRef.current.push(el)}}}>
-      <p>Testing again</p>
+    sectionRef.current.forEach((section, i) => {
+      if (i === 0) return;
+      tl.from(section, {
+        xPercent: 100,
+        opacity: 0,
+        duration: 2,
+        ease: "none",
+      });
+    });
+  });
+
+  return (
+    <div className={styles.home} ref={homeRef}>
+      <section
+        className={`${styles.hero} ${styles.section} ${styles.red}`}
+        ref={(el) => {
+          if (el && !sectionRef.current.includes(el)) {
+            sectionRef.current.push(el);
+          }
+        }}
+      >
+        <p>Testing again</p>
       </section>
-      <section className={`${styles.section} ${styles.blue}`} ref={(el) =>  {if(el && !containerRef.current.includes(el)){containerRef.current.push(el)}}}>
-      <p>This is test 3</p>
+      <section
+        className={`${styles.section} ${styles.blue}`}
+        ref={(el) => {
+          if (el && !sectionRef.current.includes(el)) {
+            sectionRef.current.push(el);
+          }
+        }}
+      >
+        <p>This is test 3</p>
       </section>
-      <section className={`${styles.section} ${styles.yellow}`} ref={(el) =>  {if(el && !containerRef.current.includes(el)){containerRef.current.push(el)}}}>
-      <p>This is a test</p>
+      <section
+        className={`${styles.section} ${styles.yellow}`}
+        ref={(el) => {
+          if (el && !sectionRef.current.includes(el)) {
+            sectionRef.current.push(el);
+          }
+        }}
+      >
+        <p>This is a test</p>
       </section>
     </div>
-  )
+  );
 }
