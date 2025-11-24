6c3c9e83409e0f3ac0ccd4ce63862739e359fea3
Ayo <Ayoroq@gmail.com>
2025-11-24 04:23:16 -0500

    Restructured the navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,94 +16,92 @@ export default function Home() {
 
   return (
     <>
-      <div className={styles.home}>
-        <section className={`${styles.hero} ${styles.section}`}>
-          <main className={styles.main}>
-            <h1 className={styles.title}>
-              We’re <em>farmers</em>,<em> purveyors</em>, and <em>eaters</em>
-              <br></br>of organically grown food.
-            </h1>
-            <Link className={styles.shopButton} to="/shop">
-              Browse Our Shop
-            </Link>
-          </main>
-          <section className={styles.subSection}>
-            <figure>
-              <img src={vegImage} alt="Fresh organic green vegetables" />
-              <figcaption>Fresh organic vegetables grown locally.</figcaption>
-            </figure>
+    <div className={styles.home}>
+      <section className={`${styles.hero} ${styles.section}`}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            We’re <em>farmers</em>,<em> purveyors</em>, and <em>eaters</em>
+            <br></br>of organically grown food.
+          </h1>
+          <Link className={styles.shopButton} to="/shop">
+            Browse Our Shop
+          </Link>
+        </main>
+        <section className={styles.subSection}>
+          <figure>
+            <img src={vegImage} alt="Fresh organic green vegetables" />
+            <figcaption>Fresh organic vegetables grown locally.</figcaption>
+          </figure>
 
-            <figure>
-              <img src={multiVegImage} alt="A variety of organic produce" />
-              <figcaption>
-                A colorful variety of organic produce from our farms.
-                <br />
-                <b>Central California</b> — The person who grew these was
-                located in Central California.
-              </figcaption>
-            </figure>
-          </section>
+          <figure>
+            <img src={multiVegImage} alt="A variety of organic produce" />
+            <figcaption>
+              A colorful variety of organic produce from our farms.
+              <br />
+              <b>Central California</b> — The person who grew these was located
+              in Central California.
+            </figcaption>
+          </figure>
         </section>
-        <section className={`${styles.second} ${styles.section}`}>
-          <section className={`${styles.secondPage}`}>
-            <video
-              src={ArialViewVideo}
-              autoPlay
-              muted
-              loop
-              type="video/mp4"
-            ></video>
+      </section>
+      <section className={`${styles.second} ${styles.section}`}>
+        <section className={`${styles.secondPage}`}>
+          <video
+            src={ArialViewVideo}
+            autoPlay
+            muted
+            loop
+            type="video/mp4"
+          ></video>
 
-            <div className={styles.info}>
-              <h2>WHAT WE BELIEVE</h2>
-              <p>
-                We believe in produce. Tasty produce. Produce like:
-                <br />
-                <br />
-                Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers.
-                Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese
-                eggplants. Asparagus. Artichokes—Jerusalem artichokes, too.
-                Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy.
-                Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley.
-                Dill.
-                <br />
-                <br />
-                What are we forgetting?
-                <br />
-                <br />
-                Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some,
-                “rocket”). Persian cucumbers, in addition to aforementioned
-                “normal” cucumbers. Artichokes. Zucchinis. Pumpkins. Squash
-                (what some cultures call pumpkins). Sweet potatoes and
-                potato-potatoes. Jackfruit. Monk fruit. Fruit of the Loom.
-                Fruits of our labor (this website). Sorrel. Pineapple. Mango.
-                Gooseberries. Blackberries. Tomatoes. Heirloom tomatoes. Beets.
-                Chives. Corn. Endive. Escarole, which, we swear, we’re vendors
-                of organic produce, but if you asked us to describe what
-                escaroles are...
-              </p>
-            </div>
-          </section>
+          <div className={styles.info}>
+            <h2>WHAT WE BELIEVE</h2>
+            <p>
+              We believe in produce. Tasty produce. Produce like:
+              <br />
+              <br />
+              Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers.
+              Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese
+              eggplants. Asparagus. Artichokes—Jerusalem artichokes, too.
+              Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy.
+              Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill.
+              <br />
+              <br />
+              What are we forgetting?
+              <br />
+              <br />
+              Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”).
+              Persian cucumbers, in addition to aforementioned “normal”
+              cucumbers. Artichokes. Zucchinis. Pumpkins. Squash (what some
+              cultures call pumpkins). Sweet potatoes and potato-potatoes.
+              Jackfruit. Monk fruit. Fruit of the Loom. Fruits of our labor
+              (this website). Sorrel. Pineapple. Mango. Gooseberries.
+              Blackberries. Tomatoes. Heirloom tomatoes. Beets. Chives. Corn.
+              Endive. Escarole, which, we swear, we’re vendors of organic
+              produce, but if you asked us to describe what escaroles are...
+            </p>
+          </div>
         </section>
-      </div>
-      <footer className={styles.footer}>
-        <div className={styles.footerContainer1}>
-          <div className={styles.footerContainer1a}>
+      </section>
+    </div>
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer1}>
+        <nav className={styles.footerNav}>
+          <div className={styles.footerNav1}>
             <Link to="/shop">SHOP</Link>
             <Link to="/about">WHO WE ARE</Link>
             <Link to="/#">JOURNAL</Link>
             <Link to="/contact">FIND US</Link>
           </div>
-          <div className={styles.footerContainer1a}>
+          <div className={styles.footerNav1}>
             <Link to="/#">FAQ</Link>
             <Link to="/#">RECIPES</Link>
             <Link to="/#">EVENT</Link>
             <Link to="/#">CONNECT</Link>
           </div>
-        </div>
-        <div className={styles.footerContainer2}>
-          <div className={styles.newsletter}>
-            <h3>
+        </nav>
+        <section className={styles.newsletter}>
+          <h3>
               Get updates & special <br /> announcements.
             </h3>
             <p className={styles.newsletterEmail}>
@@ -131,32 +129,10 @@ export default function Home() {
                 </span>
               </button>
             </p>
-          </div>
-        </div>
-      </footer>
+        </section>
+      </div>
+      <div className={styles.footerContainer2}></div>
+    </footer>
     </>
   );
 }
-// useGSAP(() => {
-//   const Navbar = document.querySelector("#nav");
-//   const NavbarHeight = Navbar ? Navbar.offsetHeight : 0;
-//   const tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: homeRef.current,
-//       start: `top ${NavbarHeight}px`,
-//       end: "+=" + (sectionRef.current.length - 1) * 100 + "%",
-//       scrub: true,
-//       pin: true,
-//       pinSpacing: true,
-//     },
-//   });
-
-//   sectionRef.current.forEach((section, i) => {
-//     if (i === 0) return;
-//     tl.from(section, {
-//       yPercent: 101,
-//       duration: 1,
-//       ease: "none",
-//     });
-//   });
-// });
