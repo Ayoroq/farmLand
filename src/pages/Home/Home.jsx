02d47e287c0be404a181e6b9ac0e38823a0358c1
Ayo <Ayoroq@gmail.com>
2025-11-24 01:19:29 -0500

    added back the second page and it's stylings

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const sectionRef = useRef([]);
   const homeRef = useRef(null);
 
-  return(
+  return (
     <div className={styles.home}>
       <section className={`${styles.hero} ${styles.section}`}>
         <main className={styles.main}>
@@ -43,115 +43,68 @@ export default function Home() {
           </figure>
         </section>
       </section>
-      <section className={`${styles.second} ${styles.section}`}></section>
+      <section className={`${styles.second} ${styles.section}`}>
+        <section className={`${styles.secondPage}`}>
+          <video
+            src={ArialViewVideo}
+            autoPlay
+            muted
+            loop
+            type="video/mp4"
+          ></video>
+
+          <div className={styles.info}>
+            <h4>WHAT WE BELIEVE</h4>
+            <p>
+              We believe in produce. Tasty produce. Produce like:
+              <br />
+              <br />
+              Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers.
+              Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese
+              eggplants. Asparagus. Artichokes—Jerusalem artichokes, too.
+              Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy.
+              Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill.
+              <br />
+              <br />
+              What are we forgetting?
+              <br />
+              <br />
+              Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”).
+              Persian cucumbers, in addition to aforementioned “normal”
+              cucumbers. Artichokes. Zucchinis. Pumpkins. Squash (what some
+              cultures call pumpkins). Sweet potatoes and potato-potatoes.
+              Jackfruit. Monk fruit. Fruit of the Loom. Fruits of our labor
+              (this website). Sorrel. Pineapple. Mango. Gooseberries.
+              Blackberries. Tomatoes. Heirloom tomatoes. Beets. Chives. Corn.
+              Endive. Escarole, which, we swear, we’re vendors of organic
+              produce, but if you asked us to describe what escaroles are...
+            </p>
+          </div>
+        </section>
+      </section>
     </div>
-  )
+  );
 }
-  // useGSAP(() => {
-  //   const Navbar = document.querySelector("#nav");
-  //   const NavbarHeight = Navbar ? Navbar.offsetHeight : 0;
-  //   const tl = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: homeRef.current,
-  //       start: `top ${NavbarHeight}px`,
-  //       end: "+=" + (sectionRef.current.length - 1) * 100 + "%",
-  //       scrub: true,
-  //       pin: true,
-  //       pinSpacing: true,
-  //     },
-  //   });
+// useGSAP(() => {
+//   const Navbar = document.querySelector("#nav");
+//   const NavbarHeight = Navbar ? Navbar.offsetHeight : 0;
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: homeRef.current,
+//       start: `top ${NavbarHeight}px`,
+//       end: "+=" + (sectionRef.current.length - 1) * 100 + "%",
+//       scrub: true,
+//       pin: true,
+//       pinSpacing: true,
+//     },
+//   });
 
-  //   sectionRef.current.forEach((section, i) => {
-  //     if (i === 0) return;
-  //     tl.from(section, {
-  //       yPercent: 101,
-  //       duration: 1,
-  //       ease: "none",
-  //     });
-  //   });
-  // });
-
-//   return (
-//     <div className={styles.home} ref={homeRef}>
-//       <section
-//         className={`${styles.hero} ${styles.horizontal}`}
-//         ref={(el) => {
-//           if (el && !sectionRef.current.includes(el)) {
-//             sectionRef.current.push(el);
-//           }
-//         }}
-//       >
-//         <main className={styles.main}>
-//           <h1 className={styles.title}>
-//             We’re <em>farmers</em>,<em> purveyors</em>, and <em>eaters</em>
-//             <br></br>of organically grown food.
-//           </h1>
-//           <Link className={styles.shopButton} to="/shop">
-//             Browse Our Shop
-//           </Link>
-//         </main>
-//         <section className={styles.part}>
-//           <figure>
-//             <img src={vegImage} alt="Fresh organic green vegetables" />
-//             <figcaption>Fresh organic vegetables grown locally.</figcaption>
-//           </figure>
-
-//           <figure>
-//             <img src={multiVegImage} alt="A variety of organic produce" />
-//             <figcaption>
-//               A colorful variety of organic produce from our farms.
-//               <br />
-//               <b>Central California</b> — The person who grew these was located
-//               in Central California and, er, hopefully very well-compensated.
-//             </figcaption>
-//           </figure>
-//         </section>
-//       </section>
-//       <section
-//         className={`${styles.horizontal}`}
-//         ref={(el) => {
-//           if (el && !sectionRef.current.includes(el)) {
-//             sectionRef.current.push(el);
-//           }
-//         }}
-//       >
-//         <section className={` ${styles.secondPage}`}>
-//           <video
-//             src={ArialViewVideo}
-//             autoPlay
-//             muted
-//             loop
-//             type="video/mp4"
-//           ></video>
-//           <div className={styles.info}>
-//             <h4>WHAT WE BELIEVE</h4>
-//             <p>
-//               We believe in produce. Tasty produce. Produce like:
-//               <br />
-//               <br />
-//               Apples. Oranges.Limes. Lemons. Guavas. Carrots. Cucumbers.
-//               Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese
-//               eggplants. Asparagus. Artichokes—Jerusalem artichokes, too.
-//               Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy.
-//               Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill.
-//               <br />
-//               <br />
-//               What are we forgetting?
-//               <br />
-//               <br />
-//               Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”).
-//               Persian cucumbers, in addition to aforementioned “normal”
-//               cucumbers. Artichokes. Zucchinis. Pumpkins. Squash (what some
-//               cultures call pumpkins). Sweet potatoes and potato-potatoes.
-//               Jackfruit. Monk fruit. Fruit of the Loom. Fruits of our labor
-//               (this website). Sorrel. Pineapple. Mango. Gooseberries.
-//               Blackberries. Tomatoes. Heirloom tomatoes. Beets. Chives. Corn.
-//               Endive. Escarole, which, we swear, we’re vendors of organic
-//               produce, but if you asked us to describe what escaroles are...
-//             </p>
-//           </div>
-//         </section>
-//       </section>
-//     </div>
-//   );
-// }
+//   sectionRef.current.forEach((section, i) => {
+//     if (i === 0) return;
+//     tl.from(section, {
+//       yPercent: 101,
+//       duration: 1,
+//       ease: "none",
+//     });
+//   });
+// });
