44b4861b51a96f297bcc0828c36cc7aaaadab0a6
Ayo <Ayoroq@gmail.com>
2025-11-28 15:30:58 -0500

    Started the setup of working with context for the basket

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { Outlet } from 'react-router'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
+import { Outlet } from "react-router";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import style from "./App.module.css";
+import { BasketContext } from "./context/BasketContext.jsx";
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 
-import { ReactLenis } from 'lenis/react'
-import { useEffect, useRef } from 'react'
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
 
 export default function App() {
-  const lenisRef = useRef()
-  
+  const lenisRef = useRef();
+
   useEffect(() => {
     function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000)
+      lenisRef.current?.lenis?.raf(time * 1000);
     }
-  
-    gsap.ticker.add(update)
-  
-    return () => gsap.ticker.remove(update)
-  }, [])
-  
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
     <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
       <div className={style.app}>
-        <Navbar />
-        <Outlet />
-        <Footer />
+        <BasketContext.Provider value={{ items: [] }}>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </BasketContext.Provider>
       </div>
     </ReactLenis>
-  )
+  );
 }
