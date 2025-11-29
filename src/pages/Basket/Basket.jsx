88d80b313fcdd7b54d9a3d32b9f78a462a0fabe0
Ayo <Ayoroq@gmail.com>
2025-11-29 03:13:32 -0500

    Added a header to the basket

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -6,33 +6,12 @@ export default function Basket() {
   const basket = useContext(BasketContext);
   const totalItems = basket.getTotalItems();
   return (
-    <main className={styles.basketMain}>
-      {!totalItems && (
-        <div className={styles.basketEmpty}>
-          <h2>Your basket is empty</h2>
-          <p>Start adding items to your basket</p>
-        </div>
-      )}
-      {totalItems > 0 && (
-        <div className={styles.basketContainer}>
-          <div className={styles.basketItems}>
-            {basket.basket.map((item, index) => (
-              <div className={styles.basketItem} key={index}>
-                <img src={item.image} alt={item.name} />
-                <div className={styles.basketItemDetails}>
-                  <h3>{item.name}</h3>
-                  <p>${item.price} / <span>{item.unit}</span></p>
-                  <div className={styles.basketItemQuantity}>
-                    <button onClick={() => basket.removeFromBasket(item)}>-</button>
-                    <p>{item.quantity}</p>
-                    <button onClick={() => basket.addToBasket(item)}>+</button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </main>
+    <div className={styles.basket}>
+      <header className={styles.basketHeader}>
+        <h1>Basket</h1>
+        <p>{totalItems} items</p>
+      </header>
+      <main className={styles.basketMain}></main>
+    </div>
   );
 }
